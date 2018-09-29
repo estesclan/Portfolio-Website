3abcb0250efec56c7a56ae28bc3fd986ea75ec54
Peter Estes <peternigelestes@gmail.com>
2018-09-29 13:01:15 -0400

    getting started

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
   render() {
@@ -13,9 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>DUDE, I'm IN!</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
